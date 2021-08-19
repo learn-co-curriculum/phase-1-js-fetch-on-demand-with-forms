a265ca290294393cc7811a3c14278a04db74f2ad
Caner <kurucaner@gmail.com>
2021-08-19 09:29:42 -0400

    commit tree 5ed8ab99734e3cfb904f8ba702f3e9a2f597375f

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = document.querySelector("#searchByID").value;
+    console.log(input);
+    fetch(`http://localhost:3000/movies/${input}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const title = document.querySelector("#movieDetails h4");
+        const summary = document.querySelector("#movieDetails p");
 
-document.addEventListener('DOMContentLoaded', init);
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
