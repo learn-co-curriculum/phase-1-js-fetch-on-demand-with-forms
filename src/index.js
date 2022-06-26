3a9f5e8b93a8c20cf4735a809e677730a47e5721
k-koech <kelvin.koech@student.moringaschool.com>
2022-06-26 06:38:58 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+    console.log(inputForm)
+
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const input = document.querySelector('input#searchByID');
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+    
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+        });
+        console.log(input.value);
+      });
 }
 
 document.addEventListener('DOMContentLoaded', init);
